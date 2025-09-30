80dcc877c15df4b38a0906552d923a594e29954b
bhj665 <bhj665@myy.haaga-helia.fi>
2025-09-30 22:37:19 +0300

    Näyttääks tää nyt hyvält?

--- ticketguru/src/main/java/ticketguru/demo/web/TicketRestController.java
+++ ticketguru/src/main/java/ticketguru/demo/web/TicketRestController.java
@@ -1,8 +1,57 @@
 package ticketguru.demo.web;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import ticketguru.demo.repositories.TicketRepository;
+import ticketguru.demo.domain.Ticket;
+import java.util.List;
+import java.util.Optional;
 
-@RestController  
+@RestController
+@RequestMapping("/api/tickets")
 public class TicketRestController {
+
+@Autowired
+private TicketRepository ticketrepository;
+
+@GetMapping
+public List<Ticket> getAllTickets() {
+return ticketrepository.findAll();
+}
+
+@GetMapping("/{ticketId}")
+public Optional<Ticket> getTicketByTicketId(@PathVariable Long ticketId) {
+return ticketrepository.findByTicketId(ticketId);
+}
+
+@PostMapping
+public Ticket createTicket(@RequestBody Ticket ticket) {
+return ticketrepository.save(ticket);
+}
+
+@PutMapping("/{ticketId}")
+public Ticket updateTicket(@PathVariable Long ticketId, @RequestBody Ticket updatedTicket) {
+return ticketrepository.findByTicketId(ticketId)
+.map(ticket -> {
+ticket.setTicketCode(updatedTicket.getTicketCode());
+return ticketrepository.save(ticket);
+})
+.orElseGet(() -> {
+updatedTicket.setTicketId(ticketId);
+return ticketrepository.save(updatedTicket);
+});
+}
+
+@DeleteMapping("/{ticketId}")
+public void deleteTicket(@PathVariable Long ticketId) {
+ticketrepository.deleteByTicketId(ticketId);
+}
 
 }
