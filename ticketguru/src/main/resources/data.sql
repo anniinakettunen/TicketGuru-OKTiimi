57afa9005589392cdbcab53594767c55dac33524
teppohalme <teppo.halme@myy.haaga-helia.fi>
2025-10-29 13:59:36 +0200

    data.sql täydennys

---
+++ ticketguru/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+-- Test data
+INSERT INTO event (event_name, event_location, event_city, event_date, event_description, max_number_of_tickets)
+VALUES 
+('Rock Night', 'Hartwall Arena', 'Helsinki', '2026-06-15', 'An electrifying evening of live rock music', 500),
+
+('Jazz & Chill', 'Tampere Hall', 'Tampere', '2026-07-20', 'Smooth jazz performances by top Finnish artists', 300),
+
+('Summer Beats Festival', 'Ruisrock Park', 'Turku', '2026-08-05', 'A weekend-long outdoor music festival with multiple stages', 1500),
+
+('Classical Evening', 'Finlandia Hall', 'Helsinki', '2026-09-10', 'An elegant classical concert featuring top orchestras', 400),
+
+('Indie Vibes', 'Nosturi', 'Helsinki', '2026-10-01', 'An intimate gig for indie and alternative music lovers', 250);
