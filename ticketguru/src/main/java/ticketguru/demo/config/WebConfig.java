5c14243a83150ff191dcc10c51111426891f70d7
teppohalme <teppo.halme@myy.haaga-helia.fi>
2025-10-31 15:43:09 +0200

    Lisätty CORS-tuki

--- ticketguru/src/main/java/ticketguru/demo/config/WebConfig.java
+++ ticketguru/src/main/java/ticketguru/demo/config/WebConfig.java
@@ -1,5 +1,23 @@
 package ticketguru.demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class WebConfig {
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // sallii kaikki domainit
+                        .allowedMethods("*") // sallii GET, POST, PUT, DELETE, OPTIONS
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
