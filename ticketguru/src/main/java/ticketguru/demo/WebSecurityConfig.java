e2f74c67f7247bea0b4fe4b1c5ac5b9fdb7ca5ea
Ville Kotilainen <villekotilainen03@gmail.com>
2025-11-04 17:42:49 +0200

    CorsFilterin päälle kytkentä opettajan neuvojen mukaan.

--- ticketguru/src/main/java/ticketguru/demo/WebSecurityConfig.java
+++ ticketguru/src/main/java/ticketguru/demo/WebSecurityConfig.java
@@ -27,6 +27,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
     http
+        .cors().and()
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(authorize -> authorize
             
