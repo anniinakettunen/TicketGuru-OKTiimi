4ffdb6ecd616b2813ac8fc6847e563c15f52afe2
bhj665 <bhj665@myy.haaga-helia.fi>
2025-11-12 13:13:50 +0200

    Update TicketTypeTest.java

--- ticketguru/src/test/java/ticketguru/demo/TicketTypeTest.java
+++ ticketguru/src/test/java/ticketguru/demo/TicketTypeTest.java
@@ -4,17 +4,18 @@ import org.junit.jupiter.api.Test;
 public class TicketTypeTest {
 @Test
 void testGetTicketTypeId() {
-var childsticket = new TicketType(11111111111, "childsticket", 5.0);
-assertEquals(11111111111, childsticket.getTicketTypeId());
+var childsticket = new TicketType(1, "childsticket", 5.0);
+assertEquals(1, childsticket.getTicketTypeId());
 }
 @Test
 void testGetTicketName() { 
-var adultsticket = new TicketType(2222222222, "adultsticket", 10.0);
+var adultsticket = new TicketType(2, "adultsticket", 10.0);
 assertEquals("adultsticket", adultsticket.getTicketName());
 }
 @Test
 void testGetPrice() {
-var pensionersticket = new TicketType(3333333333, "pensionersticket", 15.0);
+var pensionersticket = new TicketType(3, "pensionersticket", 15.0);
 assertEquals(15.0, pensionersticket.getPrice());
 }
 }
+
