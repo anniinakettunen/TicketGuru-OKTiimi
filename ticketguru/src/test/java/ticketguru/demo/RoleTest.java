0a58197b60207c03c562f79c895cdb69c6076e11
bhj665 <bhj665@myy.haaga-helia.fi>
2025-11-12 13:08:25 +0200

    Update RoleTest.java

--- ticketguru/src/test/java/ticketguru/demo/RoleTest.java
+++ ticketguru/src/test/java/ticketguru/demo/RoleTest.java
@@ -4,17 +4,17 @@ import org.junit.jupiter.api.Test;
 public class RoleTest {
 @Test
 void testGetRoleId() {
-var designer = new Role(11111111111, "designer", "makes instructions for actions in the project");
-assertEquals(11111111111, designer.getRoleId());
+var designer = new Role(1, "designer", "makes instructions for actions in the project");
+assertEquals(1, designer.getRoleId());
 }
 @Test
 void testGetRoleName() {
-var hardworker = new Role(2222222222, "hardworker", "responsibility of implementation in the project");
+var hardworker = new Role(2, "hardworker", "responsibility of implementation in the project");
 assertEquals("hardworker", hardworker.getRoleName());
 }
 @Test
 void testGetNotes() {
-var checker = new Role(3333333333, "checker", "makes sure that everything is done well in the project");
+var checker = new Role(3, "checker", "makes sure that everything is done well in the project");
 assertEquals("makes sure that everything is done well in the project", checker.getNotes());
 }
 }
