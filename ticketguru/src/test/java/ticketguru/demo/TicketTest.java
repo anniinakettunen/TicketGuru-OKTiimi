4d9f4baf37ebf411f7dce4c2fc98d290f8e90da0
bhj665 <bhj665@myy.haaga-helia.fi>
2025-11-12 13:13:22 +0200

    Update TicketTest.java

--- ticketguru/src/test/java/ticketguru/demo/TicketTest.java
+++ ticketguru/src/test/java/ticketguru/demo/TicketTest.java
@@ -4,27 +4,27 @@ import org.junit.jupiter.api.Test;
 public class TicketTest {
 @Test
 void testGetTicketId() {
-var first = new Ticket(11111111111, 111111111111, 11111111111, 11111111111, 11111111111);
+var first = new Ticket(1, 1, 1, 1, 1);
 assertEquals(11111111111, first.getTicketId());
 }
 @Test
 void testGetTicketCode() {
-var second = new Ticket(2222222222, 22222222222, 2222222222, 2222222222, 2222222222);
-assertEquals(22222222222, second.getTicketCode());
+var second = new Ticket(2, 2, 2, 2, 2);
+assertEquals(2, second.getTicketCode());
 }
 @Test
 void testGetTicketTypeId() {
-var third = new Ticket(3333333333, 33333333333, 3333333333, 3333333333, 3333333333);
-assertEquals(3333333333, third.getTicketTypeId());
+var third = new Ticket(3, 3, 3, 3, 3);
+assertEquals(3, third.getTicketTypeId());
 }
 @Test
 void testGetEventId() {
-var fourth = new Ticket(4444444444, 44444444444, 4444444444, 4444444444, 4444444444);
-assertEquals(4444444444, fourth.getEventId());
+var fourth = new Ticket(4, 4, 4, 4, 4);
+assertEquals(4, fourth.getEventId());
 }
 @Test
 void testGetTicketSale() {
-var fifth = new Ticket(5555555555, 55555555555, 5555555555, 5555555555, 5555555555);
-assertEquals(5555555555, fifth.getTicketSale());
+var fifth = new Ticket(5, 5, 5, 5, 5);
+assertEquals(5, fifth.getTicketSale());
 }
 }
