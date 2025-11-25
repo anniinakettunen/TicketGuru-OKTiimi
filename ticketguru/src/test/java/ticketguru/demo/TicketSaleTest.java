c7e10ef8efbde3e526078c492234335497739d56
bhj665 <bhj665@myy.haaga-helia.fi>
2025-11-25 16:44:46 +0200

    Update TicketSaleTest.java

--- ticketguru/src/test/java/ticketguru/demo/TicketSaleTest.java
+++ ticketguru/src/test/java/ticketguru/demo/TicketSaleTest.java
@@ -1,36 +1,26 @@
 package ticketguru.demo;
-
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import ticketguru.demo.domain.AppUser;
 import ticketguru.demo.domain.TicketSale;
-
 public class TicketSaleTest {
-
-    @Test
-    public void testDefaultConstructor() {
-        TicketSale sale = new TicketSale();
-        assertNotNull(sale);
-    }
-
-    @Test
-    public void testSettersAndGetters() {
-        TicketSale sale = new TicketSale();
-
-        LocalDateTime now = LocalDateTime.now();
-        sale.setDateTime(now);
-        assertEquals(now, sale.getDateTime());
-
-        BigDecimal price = new BigDecimal("29.99");
-        sale.setPrice(price);
-        assertEquals(price, sale.getPrice());
-
-        AppUser user = new AppUser();
-        sale.setUser(user);
-        assertEquals(user, sale.getUser());
-    }
+public final TicketSale one = new TicketSale(1, "2.1.2026 14:00", 100.0, 1, 1);
+@Test
+void getSaleIdIsPresentAndReturnsTheSaleId() throws Exception {
+checkMethod(one, "getSaleId", 1);
+}
+@Test
+void getDateTimeIsPresentAndReturnsTheDateTime() throws Exception {
+checkMethod(one, "getDateTime", "2.1.2026 14:00");
+}
+@Test
+void getPriceIsPresentAndReturnsThePrice() throws Exception {
+checkMethod(one, "getPrice", 100.0);
+}
+@Test
+void getUserIsPresentAndReturnsTheUser() throws Exception {
+checkMethod(one, "getUser", 1);
+}
+@Test
+void getTicketsIsPresentAndReturnsTheTickets() throws Exception {
+checkMethod(one, "getTickets", 1);
+}
 }
