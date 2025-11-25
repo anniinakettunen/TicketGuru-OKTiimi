033cc68bf07cc4a73829cde3bdc7c163acbbe34a
bhj665 <bhj665@myy.haaga-helia.fi>
2025-11-25 16:45:18 +0200

    Update TicketTest.java

--- ticketguru/src/test/java/ticketguru/demo/TicketTest.java
+++ ticketguru/src/test/java/ticketguru/demo/TicketTest.java
@@ -1,43 +1,26 @@
 package ticketguru.demo;
-
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 import ticketguru.demo.domain.Ticket;
-import ticketguru.demo.domain.TicketType;
-import ticketguru.demo.domain.Event;
-
 public class TicketTest {
-
-    @Test
-    public void testDefaultConstructor() {
-        Ticket ticket = new Ticket();
-        assertNotNull(ticket);
-        assertFalse(ticket.isUsed());
-    }
-
-    @Test
-    public void testUsedConstructor() {
-        Ticket ticket = new Ticket(true);
-        assertTrue(ticket.isUsed());
-    }
-
-    @Test
-    public void testSettersAndGetters() {
-        Ticket ticket = new Ticket();
-
-        ticket.setTicketCode(123L);
-        assertEquals(123L, ticket.getTicketCode());
-
-        TicketType type = new TicketType("Adult", 10.0);
-        ticket.setTicketTypeId(type);
-        assertEquals(type, ticket.getTicketTypeId());
-
-        Event event = new Event();
-        ticket.setEventId(event);
-        assertEquals(event, ticket.getEventId());
-
-        ticket.setUsed(true);
-        assertTrue(ticket.isUsed());
-    }
+public final Ticket first = new Ticket(1, 1, 1, 1, 1);
+@Test
+void getTicketIdIsPresentAndReturnsTheTicketId() throws Exception {
+checkMethod(first, "getTicketId", 1);
+}
+@Test
+void getTicketCodeIsPresentAndReturnsTheTicketCode() throws Exception {
+checkMethod(first, "getTicketCode", 1);
+}
+@Test
+void getTicketTypeIdIsPresentAndReturnsTheTicketTypeId() throws Exception {
+checkMethod(first, "getTicketTypeId", 1);
+}
+@Test
+void getEventIdIsPresentAndReturnsTheEventId() throws Exception {
+checkMethod(first, "getEventId", 1);
+}
+@Test
+void getTicketSaleIsPresentAndReturnsTheTicketSale() throws Exception {
+checkMethod(first, "getTicketSale", 1);
+}
 }
