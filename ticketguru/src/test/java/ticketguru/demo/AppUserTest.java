623de38778d8f401cd605a333a06e1557a7ffb43
bhj665 <bhj665@myy.haaga-helia.fi>
2025-11-25 16:43:13 +0200

    Update AppUserTest.java

--- ticketguru/src/test/java/ticketguru/demo/AppUserTest.java
+++ ticketguru/src/test/java/ticketguru/demo/AppUserTest.java
@@ -1,31 +1,38 @@
-package ticketguru.demo.domain;
-
+package ticketguru.demo;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-class AppUserTest {
-
-    @Test
-    void testSettersAndGetters() {
-        AppUser user = new AppUser();
-        Role role = new Role(1L, "ADMIN", null);
-
-        user.setId(1L);
-        user.setUsername("joku");
-        user.setPasswordHash("hashed");
-        user.setFirstname("Joku");
-        user.setLastname("Jokulainen");
-        user.setEmail("joku@example.com");
-        user.setPhone("12345678");
-        user.setRole(role);
-
-        assertEquals(1L, user.getId());
-        assertEquals("joku", user.getUsername());
-        assertEquals("hashed", user.getPasswordHash());
-        assertEquals("Joku", user.getFirstname());
-        assertEquals("Jokulainen", user.getLastname());
-        assertEquals("joku@example.com", user.getEmail());
-        assertEquals("12345678", user.getPhone());
-        assertEquals(role, user.getRole());
-    }
+import ticketguru.demo.domain.AppUser;
+public class AppUserTest {
+private final AppUser donaldtrump = new AppUser(1, "donaldtrump", "Abc12345", "Donald", "Trump", "donald.trump@gmail.com", "+358406285791", 1);
+@Test
+void getIdIsPresentAndReturnsTheId() throws Exception {
+checkMethod(donaldtrump, "getId", 1);
+}
+@Test
+void getUsernameIsPresentAndReturnsTheUsername() throws Exception {
+checkMethod(donaldtrump, "getUsername", "donaldtrump");
+}
+@Test
+void getPasswordHashIsPresentAndReturnsThePasswordHash() throws Exception {
+checkMethod(donaldtrump, "getPasswordHash", "Abc12345");
+}
+@Test
+void getFirstnameIsPresentAndReturnsTheFirstname() throws Exception {
+checkMethod(donaldtrump, "getFirstname", "Donald");
+}
+@Test
+void getLastnameIsPresentAndReturnsTheLastname() throws Exception {
+checkMethod(donaldtrump, "getLastname", "Trump");
+}
+@Test
+void getEmailIsPresentAndReturnsTheEmail() throws Exception {
+checkMethod(donaldtrump, "getEmail", "donald.trump@gmail.com");
+}
+@Test
+void getPhoneIsPresentAndReturnsThePhone() throws Exception {
+checkMethod(donaldtrump, "getPhone", "+358406285791");
+}
+@Test
+void getRoleIsPresentAndReturnsTheRole() throws Exception {
+checkMethod(donaldtrump, "getRole", 1);
+}
 }
