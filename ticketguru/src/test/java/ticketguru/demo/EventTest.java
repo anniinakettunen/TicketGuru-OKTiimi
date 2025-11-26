f94612bfd98318f3c6d3b556cbca25c19dc07e38
Anniina Kettunen <anniina.kettunen@gmail.com>
2025-11-26 14:51:42 +0200

    testattu testejä

--- ticketguru/src/test/java/ticketguru/demo/EventTest.java
+++ ticketguru/src/test/java/ticketguru/demo/EventTest.java
@@ -1,34 +1,33 @@
-package ticketguru.demo;
+import package ticketguru.demo.domain;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+
 import ticketguru.demo.domain.Event;
-public class EventTest {
-public final Event lightcarnival = new Event(1, "lightcarnival", "Finland", "Helsinki", "1.9.2026", "Occasion in Linnanmäki at the evening where there are everywhere lights like christmas lights", 500);
-@Test
-void getEventIdIsPresentAndReturnsTheEventId() throws Exception {
-checkMethod(lightcarnival, "getEventId", 1);
+ 
+class EventTest {
+ 
+    @Test
+    void testEventCreation() {
+        LocalDate date = LocalDate.now();
+ 
+        Event event = new Event(
+                "Test Event",
+                "Operaatalo",
+                "Helsinki",
+                date,
+                "Jazz event",
+                100
+        );
+ 
+        assertEquals("Test Event", event.getEventName());
+        assertEquals("Operaatalo", event.getEventLocation());
+        assertEquals("Helsinki", event.getEventCity());
+        assertEquals(date, event.getEventDate());
+        assertEquals("Jazz event", event.getEventDescription());
+        assertEquals(100, event.getMaxNumberOfTickets());
+    }
 }
-@Test
-void getEventNameIsPresentAndReturnsTheEventName() throws Exception {
-checkMethod(lightcarnival, "getEventName", "lightcarnival");
-}
-@Test
-void getEventLocationIsPresentAndReturnsTheEventLocation() throws Exception {
-checkMethod(lightcarnival, "getEventLocation", "Finland");
-}
-@Test
-void getEventCityIsPresentAndReturnsTheEventCity() throws Exception {
-checkMethod(lightcarnival, "getEventCity", "Helsinki");
-}
-@Test
-void getEventDateIsPresentAndReturnsTheEventDate() throws Exception {
-checkMethod(lightcarnival, "getEventDate", "1.9.2026");
-}
-@Test
-void getEventDescriptionIsPresentAndReturnsTheEventDescription() throws Exception {
-checkMethod(lightcarnival, "getEventDescription", "Occasion in Linnanmäki at the evening where there are everywhere lights like christmas lights");
-}
-@Test
-void getMaxNumberOfTicketsIsPresentAndReturnsTheMaxNumberOfTickets() throws Exception {
-checkMethod(lightcarnival, "getMaxNumberOfTickets", 500);
-}
-}
+ 
